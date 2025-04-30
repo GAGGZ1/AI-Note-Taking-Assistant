62a122f12ac012c4aabb097a3ba141b7c62935f1
Gagan Chauhan <81284966+GAGGZ1@users.noreply.github.com>
2025-05-01 01:07:44 +0530

    Initial commit

--- note-app/next.config.ts
+++ note-app/next.config.ts
@@ -5,6 +5,12 @@ const nextConfig = {
   env: {
     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
   },
+  typescript:{
+    ignoreBuildErrors:true,
+  },
+  eslint:{
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
